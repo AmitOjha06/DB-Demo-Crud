3e5f3d11fd8bfce1edc5a9bc505ebe6f0f6325cd
Amit Ojha <amit.ojha@geminisolutions.com>
2022-07-25 15:50:09 +0530

    Swagger docs updation and also test cases updation for controller.

--- src/main/java/com/demo/db/crud/controller/EmployeeController.java
+++ src/main/java/com/demo/db/crud/controller/EmployeeController.java
@@ -4,7 +4,15 @@ import com.demo.db.crud.beans.EmployeeRequest;
 import com.demo.db.crud.beans.Response;
 import com.demo.db.crud.service.EmployeeService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,38 +20,101 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/db-demo/employee")
-@OpenAPIDefinition(info = @Info (title = "Employee Controller", description = "Employee details Rest API"))
+@OpenAPIDefinition(info = @Info(title = "Employee Controller", description = "Employee details Rest API",
+        termsOfService = "Gemini Demo Terms&Services",
+        contact = @Contact(name = "Amit Ojha", url = "localhost:8080", email = "amit.ojha@geminisolutions.com"),
+        license = @License(name = "Apache 2.0", url = "license@apache.com")))
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
+    @Operation(summary = "Get All employee list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content)})
     @GetMapping("/all")
     public ResponseEntity<Response> getAllEmployees() {
         Response response = employeeService.getAllEmployees();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get employee by Emp-Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content)})
     @GetMapping("/emp-id")
-    public ResponseEntity<Response> getEmployeeById(@RequestParam(name = "Emp-Id") int empId) {
+    public ResponseEntity<Response> getEmployeeById(@Parameter(description = "id of employee to be searched")
+            @RequestParam(name = "Emp-Id") int empId) {
         Response response = employeeService.getEmployeeById(empId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Add employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "201", description = "Created",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content)})
     @PostMapping("/add")
     public ResponseEntity<Response> addEmployee(@RequestBody EmployeeRequest employeeRequest) {
         Response response = employeeService.addEmployee(employeeRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update employee by Emp-Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content)})
     @PutMapping("/update/emp-id")
-    public ResponseEntity<Response> updateEmployee(@RequestParam(name = "Emp-Id") int empId, @RequestBody EmployeeRequest employeeRequest) {
+    public ResponseEntity<Response> updateEmployee(@Parameter(description = "id of employee to be searched")
+                                                       @RequestParam(name = "Emp-Id") int empId, @RequestBody EmployeeRequest employeeRequest) {
         Response response = employeeService.updateEmployee(empId, employeeRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete employee by Emp-Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content)})
     @DeleteMapping("/delete/emp-id")
-    public ResponseEntity<Response> addEmployee(@RequestParam(name = "Emp-Id") int empId) {
+    public ResponseEntity<Response> deleteEmployee(@Parameter(description = "id of employee to be searched")
+                                                       @RequestParam(name = "Emp-Id") int empId) {
         Response response = employeeService.deleteEmployee(empId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/test/java/com/demo/db/crud/controller/EmployeeControllerTest.java
+++ src/test/java/com/demo/db/crud/controller/EmployeeControllerTest.java
@@ -2,6 +2,7 @@ package com.demo.db.crud.controller;
 
 import com.demo.db.crud.beans.EmployeeRequest;
 import com.demo.db.crud.beans.Response;
+import com.demo.db.crud.model.Employee;
 import com.demo.db.crud.service.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {EmployeeController.class})
@@ -29,38 +34,57 @@ class EmployeeControllerTest {
 
     @Test
     void testGetAllEmployees() throws Exception {
+        Response response = new Response();
+        List<Employee> employeeList = new ArrayList<>();
+        response.setStatusCode(200);
+        response.setStatusMessage("Successfully getting all details");
+        response.setResult(employeeList);
         when(employeeService.getAllEmployees()).thenReturn(new Response());
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/db-demo/employee/all");
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        assertEquals("Successfully getting all details", response.getStatusMessage());
+        assertEquals(employeeList, response.getResult());
     }
 
     @Test
     void testGetEmployeeById() throws Exception {
+        Employee employee = new Employee(1,101,"Amit","SE-2",40000.90);
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setStatusMessage("Successfully getting the details");
+        response.setResult(employee);
         when(employeeService.getEmployeeById(anyInt())).thenReturn(new Response());
         MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/api/db-demo/employee/emp-id");
         MockHttpServletRequestBuilder requestBuilder = getResult.param("Emp-Id", String.valueOf(1));
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().is(200));
+        assertEquals("Successfully getting the details", response.getStatusMessage());
+        assertEquals(employee, response.getResult());
     }
 
     @Test
     void testDeleteEmployee() throws Exception {
+        Response response = new Response();
+        response.setStatusMessage("Successfully delete the details");
         when(employeeService.deleteEmployee(anyInt())).thenReturn(new Response());
         MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/api/db-demo/employee/delete/emp-id");
         MockHttpServletRequestBuilder requestBuilder = deleteResult.param("Emp-Id", String.valueOf(1));
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().is(200));
+        assertEquals("Successfully delete the details", response.getStatusMessage());
     }
 
     @Test
     void testAddEmployee() throws Exception {
+        Response response = new Response();
+        response.setStatusMessage("Successfully Added the details");
         when(employeeService.addEmployee(any())).thenReturn(new Response());
 
         EmployeeRequest employeeRequest = new EmployeeRequest();
@@ -68,6 +92,7 @@ class EmployeeControllerTest {
         employeeRequest.setEmpId(123);
         employeeRequest.setEmpName("Emp Name");
         employeeRequest.setSalary(10.0d);
+        response.setResult(employeeRequest);
         String content = (new ObjectMapper()).writeValueAsString(employeeRequest);
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/db-demo/employee/add")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -75,11 +100,16 @@ class EmployeeControllerTest {
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().is(200));
+        assertEquals("Successfully Added the details", response.getStatusMessage());
+        assertEquals(employeeRequest, response.getResult());
     }
 
     @Test
     void testUpdateEmployee() throws Exception {
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setStatusMessage("Successfully Updated the details");
         when(employeeService.updateEmployee(anyInt(), any())).thenReturn(new Response());
 
         EmployeeRequest employeeRequest = new EmployeeRequest();
@@ -87,6 +117,7 @@ class EmployeeControllerTest {
         employeeRequest.setEmpId(123);
         employeeRequest.setEmpName("Emp Name");
         employeeRequest.setSalary(10.0d);
+        response.setResult(employeeRequest);
         String content = (new ObjectMapper()).writeValueAsString(employeeRequest);
         MockHttpServletRequestBuilder putResult = MockMvcRequestBuilders.put("/api/db-demo/employee/update/emp-id");
         MockHttpServletRequestBuilder requestBuilder = putResult.param("Emp-Id", String.valueOf(1))
@@ -95,7 +126,9 @@ class EmployeeControllerTest {
         MockMvcBuilders.standaloneSetup(employeeController)
                 .build()
                 .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().is(200));
+                .andExpect(MockMvcResultMatchers.status().is(response.getStatusCode()));
+        assertEquals("Successfully Updated the details", response.getStatusMessage());
+        assertEquals(employeeRequest, response.getResult());
     }
 }
 
